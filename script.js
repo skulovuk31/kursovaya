8ba656d4d412dc8221e247b22f849d6ffee79aba
skulovuk31 <74088173+skulovuk31@users.noreply.github.com>
2025-01-23 12:41:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('input, textarea').forEach(element => {
+        element.setAttribute('placeholder', ' ');
+    });
+
+
+
+
+
+    const form = document.getElementById('contactForm');
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+
+        console.log('Отправка формы:', { name, email, message });
+    });
+});
